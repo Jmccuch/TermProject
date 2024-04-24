7c2532815044a48ae1186c006ef3886d93e73c72
Gianni Galdo <gianni803@icloud.com>
2024-04-23 22:51:43 -0400

    Matches and Date Requests

---
+++ TermProject/Controllers/MatchesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TermProjectAPI;
+
+namespace TermProject.Controllers
+{
+    public class MatchesController : Controller
+    {
+
+        string username = "Jmccuch";
+
+        API api = new API();
+
+        //global list of pontential matches
+        List<User> potentialMatches = new List<User>();
+        public IActionResult Index()
+        {
+            potentialMatches = api.GetUserPotentialMatches(username);
+
+            return View(potentialMatches);
+        }
+
+        public IActionResult ViewProfile(string username)
+        {
+            System.Diagnostics.Debug.WriteLine("main un: " + username);
+
+            return RedirectToAction("Index", "Profile", new { username = username });
+        }
+
+        public IActionResult DateRequest(string username)
+        {
+            System.Diagnostics.Debug.WriteLine("main un: " + username);
+
+            return RedirectToAction("Index", "DateRequest", new { username = username });
+        }
+
+        public IActionResult RedirectLogOut()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
+    }
+}
