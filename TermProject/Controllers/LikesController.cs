85c8475d2710cc715f921786eabaf3367aed12f8
Gianni Galdo <gianni803@icloud.com>
2024-04-23 21:19:56 -0400

    Likes Styling

--- TermProject/Controllers/LikesController.cs
+++ TermProject/Controllers/LikesController.cs
@@ -1,13 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TermProjectAPI;
 
 namespace TermProject.Controllers
 {
     public class LikesController : Controller
     {
+        string username = "Jmccuch";
+
+        API api = new API();
+
+        //global list of pontential matches
+        List<User> potentialMatches = new List<User>();
+
+      
+
         public IActionResult Index()
         {
-            return View();
+            potentialMatches = api.GetUserPotentialMatches(username);
+
+            return View(potentialMatches);
         }
+
+        public IActionResult ViewProfile(string username)
+        {
+            System.Diagnostics.Debug.WriteLine("main un: " + username);
+
+            return RedirectToAction("Index", "Profile", new { username = username });
+        }
+
         public IActionResult RedirectLogOut()
         {
             return View("~/Views/Home/Index.cshtml");
